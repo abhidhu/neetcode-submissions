0bb64553d6cafcb27ac604a2326c9b36c6419bc8
abhidhu <95225142+abhidhu@users.noreply.github.com>
2026-05-01 14:24:39 +0530

    Add: minimum-window-with-characters - submission-3

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-3.java
@@ -0,0 +1,37 @@
+class Solution {
+    public String minWindow(String s, String t) {
+        if(t.length()> s.length()){
+            return "";
+        }
+        Map<Character, Integer> map = new HashMap<>();
+        Map<Character, Integer> temp = new HashMap<>();
+        for(int i=0; i<t.length(); i++){
+            map.put(t.charAt(i), map.getOrDefault(t.charAt(i), 0) + 1);
+        }
+        int targetMatch = map.size();
+        String ans = "";
+        int match = 0;
+        int j=0;
+        int i=0;
+        while(j<s.length()){
+            char c = s.charAt(j);
+            temp.put(c, temp.getOrDefault(c, 0)+1);
+            if(map.get(c) != null && map.get(c).equals(temp.get(c))) {
+                match ++;
+            } 
+            while(match == targetMatch ){
+                if (ans.isEmpty() || ans.length() > j - i + 1){
+                    ans = s.substring(i, j + 1);
+                }
+                c = s.charAt(i);
+                temp.put(c, temp.getOrDefault(c, 0) - 1);
+                 if(map.get(c) != null && map.get(c).equals(temp.get(c) + 1)) {
+                    match--;
+                }  
+                i++;              
+            }
+            j++;
+        }
+        return ans;
+    }
+}
