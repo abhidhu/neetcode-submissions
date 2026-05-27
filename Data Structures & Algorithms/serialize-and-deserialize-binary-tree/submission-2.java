5fd84aa32dad75184e228d1808064758fdb62257
abhidhu <95225142+abhidhu@users.noreply.github.com>
2026-05-27 23:03:18 +0530

    Add: serialize-and-deserialize-binary-tree - submission-2

---
+++ Data Structures & Algorithms/serialize-and-deserialize-binary-tree/submission-2.java
@@ -0,0 +1,42 @@
+public class Codec {
+    private int i=0;
+    // Encodes a tree to a single string.
+    public String serialize(TreeNode root) {
+        StringBuilder sb = new StringBuilder();
+        serializeHelper(root, sb);
+        if (sb.length() > 0) {
+            sb.setLength(sb.length() - 1); 
+        }
+        return sb.toString();
+    }
+
+    private void serializeHelper(TreeNode root, StringBuilder sb) {
+        if (root == null) {
+            sb.append("null,");
+            return;
+        }
+        sb.append(root.val).append(",");
+        serializeHelper(root.left, sb);
+        serializeHelper(root.right, sb);
+    }
+
+    // Decodes your encoded data to tree.
+    public TreeNode deserialize(String data) {
+        if (data == null || data.isEmpty()) return null;
+        i=0;
+        String[] nodes = data.split(",");
+        return deserializeHelper(nodes);
+    }
+
+    private TreeNode deserializeHelper(String[] nodes) {
+        String val = nodes[i++];
+        
+        if ("null".equals(val)) {
+            return null;
+        }
+        TreeNode node = new TreeNode(Integer.parseInt(val));
+        node.left = deserializeHelper(nodes);
+        node.right = deserializeHelper(nodes);
+        return node;
+    }
+}
